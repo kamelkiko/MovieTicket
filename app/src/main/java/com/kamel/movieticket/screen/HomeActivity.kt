212e7c9138c6d6bde5eb5705640aaa098de00f3e
kamel <kikokeko10@gmail.com>
2023-07-06 20:03:26 +0300

    add application class

--- app/src/main/java/com/kamel/movieticket/screen/HomeActivity.kt
+++ app/src/main/java/com/kamel/movieticket/screen/HomeActivity.kt
@@ -1,4 +1,4 @@
-package com.kamel.movieticket
+package com.kamel.movieticket.screen
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,13 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.kamel.movieticket.ui.theme.MovieTicketTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +21,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieTicketTheme {
-        Greeting("Android")
     }
 }
