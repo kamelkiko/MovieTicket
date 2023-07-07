900dfe1e0414635c7254a25030b479a768afea59
kamel <kikokeko10@gmail.com>
2023-07-07 13:51:49 +0300

    finish home screen

--- app/src/main/java/com/kamel/movieticket/screen/HomeScreen.kt
+++ app/src/main/java/com/kamel/movieticket/screen/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.kamel.movieticket.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,15 +9,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -63,6 +65,9 @@ private fun HomeContent(
     isSelectedChip: Boolean,
     onClickChip: () -> Unit,
 ) {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items =
+        listOf(R.drawable.video_player, R.drawable.search, R.drawable.ticket, R.drawable.profile)
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -111,42 +116,59 @@ private fun HomeContent(
             SpacerVertical(space = 32)
             GenericChips()
             Spacer(modifier = Modifier.weight(1f))
-            BottomAppBar(
+            NavigationBar(
                 modifier = Modifier.fillMaxWidth(),
                 containerColor = Color.Transparent,
-                contentColor = Color.Black
+                tonalElevation = 0.dp,
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.video_player),
-                        contentDescription = "movie video playing",
-                        tint = Color.White,
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .background(Orange)
-                            .padding(8.dp)
-                    )
-                    Icon(
-                        painter = painterResource(id = R.drawable.search),
-                        contentDescription = "movie search",
-                        tint = Color.Black
-                    )
-                    Icon(
-                        painter = painterResource(id = R.drawable.ticket),
-                        contentDescription = "movie ticket",
-                        tint = Color.Black
-                    )
-                    Icon(
-                        painter = painterResource(id = R.drawable.profile),
-                        contentDescription = "profile logo",
-                        tint = Color.Black
+                items.forEachIndexed { index, _ ->
+                    NavigationBarItem(
+                        icon = { Icon(painter = painterResource(id = items[index]), null) },
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color.White,
+                            unselectedIconColor = Color.Black,
+                            indicatorColor = Orange,
+                        ),
                     )
                 }
             }
+//            BottomAppBar(
+//                modifier = Modifier.fillMaxWidth(),
+//                containerColor = Color.Transparent,
+//                contentColor = Color.Black
+//            ) {
+//                Row(
+//                    modifier = Modifier.fillMaxWidth(),
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.SpaceAround
+//                ) {
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.video_player),
+//                        contentDescription = "movie video playing",
+//                        tint = Color.White,
+//                        modifier = Modifier
+//                            .clip(CircleShape)
+//                            .background(Orange)
+//                            .padding(8.dp)
+//                    )
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.search),
+//                        contentDescription = "movie search",
+//                        tint = Color.Black
+//                    )
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.ticket),
+//                        contentDescription = "movie ticket",
+//                        tint = Color.Black
+//                    )
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.profile),
+//                        contentDescription = "profile logo",
+//                        tint = Color.Black
+//                    )
+//                }
         }
     }
 }
